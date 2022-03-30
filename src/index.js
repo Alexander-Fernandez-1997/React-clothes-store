275c6672d55d47cb986add0f4c188cfe09577790
KingAlex1997 <alex97l@hotmail.com>
2022-03-30 19:44:48 -0300

    cambios a 0 , sacamos chakra ahora vamos a trabajar con una template y bootstrap

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { App } from "./App";
 import "./index.css";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
