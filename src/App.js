479b187f9b3c6811aca164e20303f57cdf6a71b6
KingAlex1997 <alex97l@hotmail.com>
2022-04-27 18:27:54 -0300

    bye cmsbutter , uninstalled

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@ import { useState, useEffect } from "react";
 import { Cartas } from "./components/Cartas";
 import { Banner } from "./components/Banner";
 import { db } from "./firebase/firestore";
-// import Butter from "buttercms";
 
-// const butter = Butter(process.env.REACT_APP_BUTTER_ECOMMERCE);
 
 export const App = () => {
   const [products, setProducts] = useState([]);
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const res = await butter.content.retrieve(["clothes"], {
-  //       order: "name",
-  //     });
-  //     const { data } = await res.data;
-  //     const allProducts = data.clothes;
-  //     setProducts(allProducts);
-  //   }
-  //   fetchData();
-  // }, []);
+ 
   useEffect(() => {
     db.collection("clothes").onSnapshot((snapshot) =>
       setProducts(
